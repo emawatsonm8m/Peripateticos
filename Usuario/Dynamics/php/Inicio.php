7242b525432829b45243a9249651a13995263a88
m&m <emwatsrl21@gmail.com>
2023-06-16 16:22:39 -0600

    agrego parte de la página de inicio

---
+++ Usuario/Dynamics/php/Inicio.php
@@ -0,0 +1,85 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Red Coyote</title>
+    <link rel="stylesheet" href="../../Statics/styles/Inicio.css">
+</head>
+<body>
+    <nav>
+        <section class="img">
+            <img src="../../Statics/media/EscudoP6.jpg" alt="ENP6" id="prepa6">
+        </section>
+        <section class="pags">
+            <article class="pagRedireccion"><button class="boton" >Ventas</button></article>
+            <article class="pagRedireccion"><button class="boton" >Objetos perdidos</button></article>
+            <article class="pagRedireccion"><button class="boton" >Foro</button></article>
+            <article class="fotoUsuario"><img></article> <!--hacer alguna variable para que se guarde la imágen que suba el ususario en el form-->
+        </section>
+    </nav>
+    <header>
+        <h1>Red Coyote</h1>
+    </header>
+
+    <main>
+        <div class="CARRUSEL">
+        <div class="carrusel">
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+
+            <div class="elemento">
+                <img><!--Insertar imagen-->
+            </div>
+        </div>
+        </div>
+            <h2>Nuestra misión</h2>
+        <aside>
+            <p>El prósito de esta nueva red es vincular a la comunidad ENP6 de la UNAM, tratando de mejorar la experiencia de navegación para cada usuario, y manteniéndo a la comunidad informada tanto de loe eventos formales y no formales que se llevan a cabo, queremos fomentar la inclusión y participación de los alumnos, en ayuda a la misma comunidad, y darle una mayor visibilidad a diferentes tipos de grupos estudiantiles que existen en la prepa que pueden ser de interés, pero que no tenían tanta difusión, con esto te damos la bienvenida a esta hermosa Red Coyote.</p>
+        </aside>
+    </main>
+</body>
+</html>
