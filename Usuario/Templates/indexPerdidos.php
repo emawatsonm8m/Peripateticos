50b828a52c8677c8526fa82454b64f28cf773c78
MaryIsaKore <maria.isa.kore@gmail.com>
2023-06-17 16:28:39 -0600

    Feat:Agrega archivos de ObjetosPerdidos

---
+++ Usuario/Templates/indexPerdidos.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Objetos Perdidos</title>
+</head>
+<body>
+<nav class="NavBar">
+        <ul>
+            <li><img src="../Statics/media/EscudoP6.png" id="P6" alt="Prepa_6">
+            <img src="../Statics/media/EscudoUNAM.png" id="UNAM"alt="UNAM"></li>
+            <li><a href="#" class="TextSelect">Ventas</a></li>
+            <li><a href="#" class="TextSelect">Objetos perdidos</a></li>
+            <li><a href="#" class="TextSelect">Comunidad P6</a></li>
+            <li><a href="#"><img src="../Statics/media/PerfilVacio.png" id="FP" alt="FotoPerfil"></a></li>
+        </ul>
+    </nav>
+    <header>
+        <h1>Objetos Perdidos</h1>
+    </header>
+
+    <section>
+        <h2>Publicaciones Recientes</h2>
+        <div>
+            <button>Filtrar por</button>
+        </div>
+        <div>
+            <input type="search" placeholder="Buscar" size="317px">
+        </div>
+        <!--en esta parte se deven desplegar los divs de los objetos-->
+    </section>
+    <div>
+        <h2 class="AgregarObjeto">¿Perdiste Algo?</h2>
+        <button></button>
+    </div>
+    <div>
+        <h2 class="AgregarObjeto">¿Encontraste Algo?</h2>
+        <button></button>
+    </div>
+    <div class="Calendario">
+        <!--deja que confirme-->
+    </div>
+    
+</body>
+</html>
