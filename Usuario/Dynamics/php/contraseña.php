0bab51eb813cde67cabf4bdb4e8391e9ca4b544e
Joxedy8 <321164834@alumno.enp.unam.mx>
2023-06-17 11:09:20 -0600

    feat: agrega inicio de sesion y registro

--- Usuario/Dynamics/php/contraseña.php
+++ Usuario/Dynamics/php/contraseña.php
@@ -3,30 +3,30 @@
         $contraseñaHasheada = hash("SHA256", $contra);
         return $contraseñaHasheada;
     }
-    function verificarContra($contraseña, $correcta, $sal){    //contraseña es lo que manda el usuario  //correcta y sal se saca de base de datos
+    function verificarContra ($contra, $correcta, $sal){    //contra la manda el usuario  //correcta y sal viene de base de datos
         $coincide = false;
         $caracteres = str_split("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
         for($i=0; $i<count($caracteres); $i++){
             for($j=0; $j<count($caracteres); $j++){
                 $pimienta = $caracteres[$i].$caracteres[$j];
-                $contra = $contraseña.$pimienta.$sal;
-                if(hashearContra($contra) == $correcta){
+                $contraseña = $contra.$pimienta.$sal;
+                if(hashearContra($contraseña) == $correcta){
                     $coincide = true;
                     break;
                 }
             }
         }
-        return true;
+        return $coincide;
     }
     function generarSal(){
-        $sal = uniquid();
+        $sal = uniqid();
         return $sal;
     }
     function generarPimienta(){
         $caracteres = str_split("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
         $partesPimienta = array_rand($caracteres, 2);
-        $p1 = $caracteres($partesPimienta[0]);
-        $p2 = $caracteres($partesPimienta[1]);
+        $p1 = $caracteres[$partesPimienta[0]];
+        $p2 = $caracteres[$partesPimienta[1]];
         $pimienta = $p1.$p2;
         return $pimienta;
     }
--- Usuario/Dynamics/php/verifica.php
+++ Usuario/Dynamics/php/verifica.php
@@ -1,11 +1,6 @@
 <?php
-    session_start();
-    if(!isset($_SESSION["usuario"])){
-        header('Location: ../../index.php');
-    }
-
-    require "config.php";
-    require "contraseña.php";
+    require "./config.php";
+    require "./contraseña.php";
     $conexion = connect ();
 
     if(!$conexion){
@@ -14,23 +9,23 @@
         $usuario = (isset($_POST["usuario"]) && $_POST["usuario"] != "")? $_POST["usuario"] : false;
         $contraseña = (isset($_POST["contraseña"]) && $_POST["contraseña"] != "")? $_POST["contraseña"] : false;
     
-        //si los dos datos llegan
         if($usuario && $contraseña){
             $existe = "SELECT * FROM usuario WHERE Cuenta = '$usuario'";
             $buscando = mysqli_query($conexion, $existe);
             if(mysqli_num_rows($buscando) == 0){        //verifica si existe un registro con ese usuario
                 echo "Ese usuario no existe";
             }else{
-                $sql = "SELECT Contraseña FROM Usuario WHERE Cuenta = '$usuario'";
-                $correcta = mysqli_query($conexion, $sql);
-                $sql1 = "SELECT Sal FROM Usuario WHERE Cuenta = '$usuario'";
-                $sal = mysqli_query($conexion, $sql1);
-
+                $sql = "SELECT Contraseña, Sal FROM Usuario WHERE Cuenta = '$usuario'";
+                $res = mysqli_query($conexion, $sql);
+                while($respuesta = mysqli_fetch_assoc($res)){
+                    $correcta = $respuesta['Contraseña'];
+                    $sal = $respuesta['Sal'];
+                }
                 $verificacion = verificarContra($contraseña, $correcta, $sal);
                 if($verificacion){        //verifica que la contraseña sea la misma
                     session_start();
                     $_SESSION["usuario"] = $usuario;
-                    header('Location: ./Dynamics/php/...');
+                    header('Location: ./comunidad.php'); //ruta momentanea
                 }else{
                     echo "Contraseña incorrecta";
                 }
