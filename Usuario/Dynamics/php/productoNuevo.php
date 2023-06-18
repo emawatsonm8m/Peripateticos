825a9e8ecfdbac0185d0afbfb588a447f99c1fd1
Luana <lsalvarezmolina@gmail.com>
2023-06-18 15:42:15 -0600

    feat: añade vista y registros oficiales de puntosprepa

---
+++ Usuario/Dynamics/php/productoNuevo.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../libs/bootstrap-5.3.0-dist (1)/bootstrap-5.3.0-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../../Statics/styles/productoNuevo.css">
+    <link rel="stylesheet" href="../../Statics/styles/navs.css">
+    <title>Producto</title>
+</head>
+<body>
+    <!-- NAV -->
+    <nav>
+      <section class="img">
+          <img src="../../Statics/media/EscudoP6.jpg" alt="ENP6" id="prepa6">
+          <img src="../../Statics/media/EscudoUNAM.jpg" alt="UNAM" id="UNAM">
+      </section>
+      <section class="pags">
+          <article class="pagRedireccion" id="pagPrincipal">Pagina principal</article>
+          <article class="pagRedireccion" id="ventas">Ventas</article>
+          <article class="pagRedireccion" id="objPerdidos">Objetos perdidos</article>
+          <article class="pagRedireccion" id="comunidad">Comunidad P6</article>
+          <article class="fotoUsuario" id="perfil"><!--Foto de usuario BD--></article>
+      </section>
+  </nav>
+  <!-- CONTENIDO -->
+  <main>
+    <aside>
+        <article id="mapayhorarios">
+            <h3>Punto de venta y horario</h3>
+            <div id="mapa"></div>
+            <div id="horario">Horario:</div>
+            <div class="leyenda">Este punto de venta y horario fueron designados por el vendedor, si no se adecuan a tu disponibilidad recuerda que puedes contactar al vendedor y acordar la entrega en un punto/horario distinto</div>
+        </article>
+        <article id="contacto">
+            <h3>Contacta al vendedor</h3>
+            <div id="celular">
+                <img src="../../Statics/media/publicarProducto/cel.png" class="img-fluid" id="cel">
+                <div class="texto">55</div> 
+            </div>
+            <div id="instagram">
+                <img src="../../Statics/media/publicarProducto/ig.png" class="img-fluid" id="ig">
+                <div class="texto">@</div>
+            </div>
+        </article>  
+    </aside>  
+    <section class="producto">
+
+    </section>
+  <main>
+</body>
+</html>
--- Usuario/Dynamics/php/publicarProducto.php
+++ Usuario/Dynamics/php/publicarProducto.php
@@ -5,59 +5,95 @@
     //     header('Location: ../../index.php');
     // }
 
-    
-    $nombre = (isset($_POST["nombre"]) && $_POST["nombre"] != "")? $_POST["nombre"] : false;
-    $arch = (isset($_POST["arch"]) && $_POST["arch"] != "")? $_POST["arch"] : false;
-    $descripcion = (isset($_POST["descripcion"]) && $_POST["descripcion"] != "")? $_POST["descripcion"] : false;
-    $costo = (isset($_POST["costo"]) && $_POST["costo"] != "")? $_POST["costo"] : false;
-    $fecha = (isset($_POST["fecha"]) && $_POST["fecha"] != "")? $_POST["fecha"] : false;
-    $lugar = (isset($_POST["lugar"]) && $_POST["lugar"] != "")? $_POST["lugar"] : false;
-    $hora = (isset($_POST["hora"]) && $_POST["hora"] != "")? $_POST["hora"] : false;
-
-    if($_FILES["arch"])
+    require "config.php";
+    $conexion = connect();
+    if(!$conexion)
     {
-            $arch = $_FILES["arch"];
-            $name = $arch["name"];
-            $ruta_temp = $arch["tmp_name"];
-            $ext = pathinfo($name, PATHINFO_EXTENSION);
-            if(!file_exists('../../Statics/img'))
-            {
-                mkdir('../../Statics/img');
-            }
-            $nuevaRuta= "../../Statics/img/$name";
-    }
-
-    if($nombre && $arch && $descripcion && $costo && $fecha && $lugar && $hora)
+        echo "No se pudo conectar con la base";
+    }else
     {
-        ///$respuesta = array("nombre"=>$nombre, "archivo"=>$nuevaRuta, "descripcion"=>$descripcion, "costo"=>$costo, "fecha"=>$fecha, "lugar"=>$lugar, "hora"=>$hora);
-        if(!$peticion)
-            {
-                $respuesta = array("ok"=>false, "mensaje" => "No se pudo publicar tu producto. Intentálo de nuevo más tarde.");
-            } else{
-                $respuesta = array("ok"=>true, "mensaje" => "Producto publicado. Ahora lo puedes ver en la opción tus productos.");
-            }
-        echo json_encode($respuesta);
-    }
+        $nombre = (isset($_POST["nombre"]) && $_POST["nombre"] != "")? $_POST["nombre"] : false;
+        $arch = (isset($_POST["arch"]) && $_POST["arch"] != "")? $_POST["arch"] : false;
+        $descripcion = (isset($_POST["descripcion"]) && $_POST["descripcion"] != "")? $_POST["descripcion"] : false;
+        $costo = (isset($_POST["costo"]) && $_POST["costo"] != "")? $_POST["costo"] : false;
+        $fecha = (isset($_POST["fecha"]) && $_POST["fecha"] != "")? $_POST["fecha"] : false;
+        $lugar = (isset($_POST["lugar"]) && $_POST["lugar"] != "")? $_POST["lugar"] : false;
+        $hora = (isset($_POST["hora"]) && $_POST["hora"] != "")? $_POST["hora"] : false;
 
-    else 
-    {
-        if(!$nombre)
-            $respuesta = array("ok"=>false, "mensaje" => "No se especificó el Nombre del producto.");
-        else if ( !$arch){
-            $respuesta = array("ok"=>false, "mensaje" => "No se especificó la foto del producto.");
-        }else if ( !$descripcion){
-            $respuesta = array("ok"=>false, "mensaje" => "No se especificó la descripción del producto.");
-        }else if ( !$costo){
-            $respuesta = array("ok"=>false, "mensaje" => "No se especificó el costo del producto.");
-        }else if(!$fecha){
-            $respuesta = array("ok"=>false, "mensaje" => "No se especificó la fecha de venta del producto.");
-        }else if(!$lugar){
-            $respuesta = array("ok"=>false, "mensaje" => "No se especificó el lugar de entrega del producto.");
-        }else if(!$hora){
-            $respuesta = array("ok"=>false, "mensaje" => "No se especificó el horario disponible para la entrega.");
+        if($_FILES["arch"])
+        {
+                $arch = $_FILES["arch"];
+                $name = $arch["name"];
+                $ruta_temp = $arch["tmp_name"];
+                $ext = pathinfo($name, PATHINFO_EXTENSION);
+                if(!file_exists('../../Statics/img'))
+                {
+                    mkdir('../../Statics/img');
+                }
+                $nuevaRuta= "../../Statics/img/$name";
         }
-        echo json_encode($respuesta);
-    }
+
+        if($lugar==true)
+        {
+            if($lugar=="Entrada")
+                $lugar = 1;
+            else if ($lugar=="Patio de Sextos")
+                    $lugar = 2;
+            else if ($lugar=="Patio de Cuartos")
+                    $lugar = 3;
+            else if ($lugar=="Patio de Quintos")
+                    $lugar = 4;
+            else if ($lugar=="Pulpo")
+                    $lugar = 5;
+            else if ($lugar=="Mesas")
+                    $lugar = 6;
+            else if ($lugar=="Canchas")
+                    $lugar = 7;
+            else if ($lugar=="Pimponeras")
+                    $lugar = 8;
+            else if ($lugar=="Los H")
+                    $lugar = 9;
+            else if ($lugar=="Biblioteca")
+                    $lugar = 10;
+            else if ($lugar=="Vestidores")
+                    $lugar = 11;
+        }
+        
+        if($nombre && $nuevaRuta && $descripcion && $costo && $fecha && $lugar && $hora)
+        {
+            $usuario = 1;
+            $sql = "INSERT INTO producto ( ID_Usuario, ID_PuntosPrepa, NombreProducto, Foto, Descripcion, Costo, FechaVenta, Horario)
+            VALUES ($usuario, $lugar,'$nombre', '$nuevaRuta', '$descripcion', $costo, '$fecha', '$hora')";
+            $res = mysqli_query($conexion, $sql);
+            //$id = mysqli_insert_id($conexion);
+            if(!$res)
+                $respuesta = array("ok"=>false, "mensaje" => "No se pudo publicar tu producto. Intentálo de nuevo más tarde.");
+            else
+                $respuesta = array("ok"=>true, "mensaje" => "Producto publicado. Ahora lo puedes ver en la opción tus productos.");
+                
+            //echo json_encode($respuesta);
+        }
+
+        else 
+        {
+            if(!$nombre)
+                $respuesta = array("ok"=>false, "mensaje" => "No se especificó el Nombre del producto.");
+            else if ( !$arch){
+                $respuesta = array("ok"=>false, "mensaje" => "No se especificó la foto del producto.");
+            }else if ( !$descripcion){
+                $respuesta = array("ok"=>false, "mensaje" => "No se especificó la descripción del producto.");
+            }else if ( !$costo){
+                $respuesta = array("ok"=>false, "mensaje" => "No se especificó el costo del producto.");
+            }else if(!$fecha){
+                $respuesta = array("ok"=>false, "mensaje" => "No se especificó la fecha de venta del producto.");
+            }else if(!$lugar){
+                $respuesta = array("ok"=>false, "mensaje" => "No se especificó el lugar de entrega del producto.");
+            }else if(!$hora){
+                $respuesta = array("ok"=>false, "mensaje" => "No se especificó el horario disponible para la entrega.");
+            }
+        }
+}
+echo json_encode($respuesta);
 
 
     
